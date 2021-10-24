b45737f894327cc8b6362f10a52cbd6db64d4505
vraiSlophil <slophil.contact@gmail.Com>
2021-10-24 10:22:09 +0200

    V2

--- src/main/java/fr/slophil/other/DailyCCReload.java
+++ src/main/java/fr/slophil/other/DailyCCReload.java
@@ -1,10 +1,12 @@
 package fr.slophil.other;
 
 import fr.slophil.FuturaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.time.LocalTime;
 
-public class DailyCCReload {
+public class DailyCCReload extends BukkitRunnable {
 
     private FuturaPlugin main;
     public DailyCCReload(FuturaPlugin futuraPlugin) {
@@ -14,8 +16,15 @@ public class DailyCCReload {
     LocalTime configTime = LocalTime.parse(main.getConfig().getString("Time"));
     LocalTime nowTime = LocalTime.now();
 
-    if (nowTime.compareTo(configTime)){
-
+    @Override
+    public void run() {
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(main, new Runnable() {
+            @Override
+            public void run() {
+                if (nowTime.compareTo(configTime) == 0){
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "cc reload");
+                }
+            }
+        },0,20*60);
     }
-
 }
